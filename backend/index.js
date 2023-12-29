2575032a6b3c33a5104fc6170ecf2b0abda3699e
Ashif788 <117247170+Ashif788@users.noreply.github.com>
2023-12-29 10:03:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 app.use(
   cors({
-    origin: "https://frontend-kappa-lovat.vercel.app",
+    origin: "http://localhost:3000/",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   })
